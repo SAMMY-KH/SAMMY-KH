d5da59b56bd57c677123756244b93f12725a5f99
SAMMY-KH <120622931+SAMMY-KH@users.noreply.github.com>
2022-12-15 13:34:32 +0700

    Update auto rename all file in folder.py
    Rename all file python 
    this full project for rename all file in path just one click include with extension mp4,mp3,jpg,png

--- auto rename all file in folder.py
+++ auto rename all file in folder.py
@@ -3,12 +3,12 @@ i = 1
 path = input('Enter Path: ')
 setName = input('Enter new name: ')
 path = path + '/'
-for filename in os.listdir(path)
+for filename in os.listdir(path):
     file_extension = filename.split('.')
     file = setName + str(i) + '.' + file_extension[1]
     Sourcefile = path + filename
     file = path + file
-    os.rename(Source + file)
+    os.rename(Sourcefile + file)
     i += 1
 print('All file was rename !')    
     
